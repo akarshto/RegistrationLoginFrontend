c9c273b2d8ee8051bbb321bd0fc16d790688171e
Akarsh T O <akarsh1064@gmail.com>
2026-09-20 19:39:26 +0530

    Fix production configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true,
+    baseURL: "https://registrationloginbackend-wtr4.onrender.com/api",
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export default api;
